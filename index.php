617b1ffccc8a77706d8be7a70dc497c0eb8dfb22
Yves Charvis <Yves Charvis>
2017-02-22 11:39:54 +0100

    FIRST

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php include 'header.html' ?>
+
+	<!--///////////////////////////////////Phrase Accorche + DIV FACEBOOK ? \\\\\\\\\\\\\\\-->
+<div class="col-md-offset-5 col-md-5 col-lg-offset-5">
+	<img id="phraseAccroche" class="dis-phrase" src="img/phrase.png" width="87%">
+</div>
+	<!--///////////////////////////////////CONTENEUR\\\\\\\\\\\\\\\\\\\\\\-->
+<div id="containerUne col-md-10 col-md-offset-1">
+	<div id="conteneur" class="col-xs-12 col-md-9"> 		<!--Conteneur detier la Une, Menu press et social -->
+		<div id="laUne" class="">
+			<H2 class="aLaUne col-lg-7 text-left text-capitalize">A <span>La Une</span></H2>  	<!-- La Une DIV -->
+			<div class="les-articles">
+				<img src="img/une1.png" alt="" class="articleimg1 col-xs-10 col-xs-offset-1 col-md-6 col-md-offset-0">
+				<div class="row">
+					<img class="articleimg2 col-xs-10 col-md-3" src="img/une3.png" width="500px" alt="">
+					<img class="articleimg3 col-xs-10 col-md-3" src="img/une3.png" width="500px" alt="">
+					<img class="articleimg4 col-xs-10 col-md-3" src="img/une3.png" width="500px" alt=""> 
+					<img class="articleimg5 col-xs-10 col-md-3" src="img/une3.png" width="500px" alt="">
+				</div>  
+			</div>	
+		</div>
+	</div>
+	<!-- /////////////////////////////////Partie Social FB \\\\\\\\\\\\\\\\-->
+<div id="fb-root" class="col-md-4" >
+    	<img class="col-md-offset-2" src="img/facebook.png" width="150">
+	
+		<iframe class="recommend" src="https://www.facebook.com/plugins/like.php?href=https%3A%2F%2Fwww.facebook.com%2Ffaceterritoirebourbonnais&width=105&layout=box_count&action=recommend&size=small&show_faces=true&share=true&height=65&appId" width="105" height="65" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true">			
+		</iframe>
+			<iframe class="pagefb" src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Ffaceterritoirebourbonnais%2F&tabs=timeline&width=340&height=500&small_header=true&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width="400" height="800" style="border:none;overflow:hidden" scrolling="no" frameborder="1" allowTransparency="true">		
+			</iframe>
+</div>
+</div> <!-- fermeture containerUne -->
+
+<script type="text/javascript" src="js/script.js"></script>
+<?php include 'footer.html' ?>
+
