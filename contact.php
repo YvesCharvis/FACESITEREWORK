0eb09eb910ce81085170b18d286d57808057959b
Eddy Huleu <eddy.huleu@live.fr>
2017-02-24 14:35:22 +0100

    contact_face_php

---
+++ contact.php
@@ -0,0 +1,61 @@
+<?php include 'header.html' ?>
+
+	<article>
+    <div id="bourb">
+        <img id="face" src="img/contact.jpg"> 
+        <p>Contacter Face Territoire Bourbonnais</p>
+        <p>3 rue Berthelot, 03000 MOULINS</p>  
+        <p>Tél : 04 70 20 43 70</p>
+    </div>
+        
+        <div id="premier">
+            <img id ="president" src="img/president.jpg"> 
+            <a class="link" href="#"> Didier Luminet 
+            <span>
+                <p> Présisent, chef d'entrepise </p>
+                <p>Tel: 06.64.02.12.99</p>
+                <p>Mail: faceterritoirebourbonnais@gmail.com</p>
+            </span>
+            </a>
+        </div>
+        
+
+        <div id="deux">
+            <img  id = "eric" src="img/eric.jpg">
+            <a class="link" href="#"> Eric Getenay 
+            <span>
+                <p>Chargé des relations entreprises</p>
+                <p>Tél : 06 89 32 71 09</p>
+                <p>Mail : fab_numerique_allier@fondationface.org</p>
+            </span>
+            </a>
+        </div>
+    
+        <div id="trois">
+            <img id ="christine" src="img/christine.jpg">
+            <a class="link" href="#"> Christine Berger
+            <span>
+                <p>Administrative et Commerciale</p>
+                <p>Tel: 06 73 65 04 95</p>
+                <p>Mail: c.berger@fondationface.org</p>
+            </span>
+            </a>
+        </div>
+
+
+        <div id="quatre">
+            <img id="xavier" src="img/xavier.jpg">
+            <a class="link" href="#"> Xavier de Soultrait
+            <span>
+                <p>Face Territoire Bourbonnais</p>
+                <p>Tel: 06 75 81 23 40</p>
+                <p>Mail: xavier.desoultrait@gmail.com</p>
+            </span>
+            </a>
+        </div>
+
+</article>
+
+
+<script type="text/javascript" src="js/script.js"></script>
+<?php include 'footer.html' ?>
