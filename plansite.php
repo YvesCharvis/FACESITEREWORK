7b5a149daeb685621439685fe9b49113c66f79a4
William <william.legal03@gmail.com>
2017-02-24 16:37:04 +0100

    up

---
+++ plansite.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="css/plan.css">
+	<title>plansite</title>
+</head>
+<body>
+
+	<div class="conteneur">
+
+		<div class="row">
+
+			<div id="rs" class="col-xs-3 col-md-3">
+				
+				<img src="img/fb.png">
+				<iframe src="http://www.facebook.com/plugins/like.php?href=http://www.example.com/page.html&layout=box_count&show_faces=true&width=65&action=like&font=arial&colorscheme=light&height=65" 
+	  			scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:65px; height:65px; margin-top:3px;" allowTransparency="true">
+	  			</iframe>
+	  			
+			</div>
+		</div>
+
+		<div class="row">
+
+			<div class="col-xs-1 col-md-1"></div>		
+
+			<div id="plancomplet" class="col-xs-10 col-md-10">
+				
+				<h3>Accueil</h3>
+					<ul>	
+					<li>Fil d'actualité</li>
+					<li>Suivez-nous !</li>
+					</ul>
+
+				<h3>Présentation</h3>	
+					<ul>	
+					<li>Histoire</li>
+					<li>Actions</li>
+					</ul>
+
+				<h3>Galerie</h3>	
+					<ul>	
+					<li>Galerie de photos</li>
+					</ul>
+
+				<h3>Partenaires</h3>	
+					<ul>	
+					<li>Liste des partenaires</li>
+					</ul>
+
+				<h3>La Fabrik</h3>	
+
+					<ul>	
+					<li>Présentation</li>
+					<li>Suivez-nous !</li>
+					<li>Galerie de photos</li>
+					</ul>
+
+				<h3>Contact</h3>	
+					<ul>	
+					<li>Contact Face</li>
+					<li>Contact LaFabrik</li>
+					</ul>
+
+			</div>
+
+			<div class="col-xs-1 col-md-1"></div>
+
+		</div>
+	</div>
+
+
+</body>
+</html>
